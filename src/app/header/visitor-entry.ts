ac2ac335d93f2bdfc10a56a3ac6662dd320a7273
Trinay5 <113661792+Trinay5@users.noreply.github.com>
2025-04-17 10:24:30 +0530

    Delete src directory

--- src/app/header/visitor-entry.ts
+++
@@ -1,6 +0,0 @@
-export interface VisitorEntry {
-    visitorId:string
-    visitorName:string
-    visitorDate:string
-    visitorTime:string
-}
--- src/app/header/visitor-list.ts
+++
@@ -1,6 +0,0 @@
-export interface VisitorList {
-    visitorId:string
-    visitorName:string
-    visitorDate:string
-    visitorTime:string
-}
