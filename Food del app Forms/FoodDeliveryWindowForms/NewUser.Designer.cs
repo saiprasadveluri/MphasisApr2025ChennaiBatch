bdef5f0d8c950b16358d272c9e48187e0fd9ac17
Sagarika1611Katkam <ksagarika1611@gmail.com>
2025-05-26 17:38:34 +0530

    Add files via upload

---
+++ Food del app Forms/FoodDeliveryWindowForms/NewUser.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace FoodDeliveryWindowForms
+{
+    partial class NewUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.id = new System.Windows.Forms.Label();
+            this.textUserId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textUserEmail = new System.Windows.Forms.TextBox();
+            this.textUserPassword = new System.Windows.Forms.TextBox();
+            this.RoleComboBox = new System.Windows.Forms.ComboBox();
+            this.SaveUsers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // id
+            // 
+            this.id.AutoSize = true;
+            this.id.Location = new System.Drawing.Point(65, 42);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(16, 13);
+            this.id.TabIndex = 0;
+            this.id.Text = "Id";
+            // 
+            // textUserId
+            // 
+            this.textUserId.Location = new System.Drawing.Point(164, 39);
+            this.textUserId.Name = "textUserId";
+            this.textUserId.Size = new System.Drawing.Size(100, 20);
+            this.textUserId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // textUserName
+            // 
+            this.textUserName.Location = new System.Drawing.Point(164, 76);
+            this.textUserName.Name = "textUserName";
+            this.textUserName.Size = new System.Drawing.Size(100, 20);
+            this.textUserName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Role";
+            // 
+            // textUserEmail
+            // 
+            this.textUserEmail.Location = new System.Drawing.Point(164, 115);
+            this.textUserEmail.Name = "textUserEmail";
+            this.textUserEmail.Size = new System.Drawing.Size(100, 20);
+            this.textUserEmail.TabIndex = 7;
+            // 
+            // textUserPassword
+            // 
+            this.textUserPassword.Location = new System.Drawing.Point(164, 151);
+            this.textUserPassword.Name = "textUserPassword";
+            this.textUserPassword.Size = new System.Drawing.Size(100, 20);
+            this.textUserPassword.TabIndex = 8;
+            // 
+            // RoleComboBox
+            // 
+            this.RoleComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.RoleComboBox.FormattingEnabled = true;
+            this.RoleComboBox.Items.AddRange(new object[] {
+            "Admin",
+            "Owner",
+            "Customer"});
+            this.RoleComboBox.Location = new System.Drawing.Point(164, 194);
+            this.RoleComboBox.Name = "RoleComboBox";
+            this.RoleComboBox.Size = new System.Drawing.Size(121, 21);
+            this.RoleComboBox.TabIndex = 9;
+            // 
+            // SaveUsers
+            // 
+            this.SaveUsers.Location = new System.Drawing.Point(143, 260);
+            this.SaveUsers.Name = "SaveUsers";
+            this.SaveUsers.Size = new System.Drawing.Size(75, 23);
+            this.SaveUsers.TabIndex = 10;
+            this.SaveUsers.Text = "SaveUsers";
+            this.SaveUsers.UseVisualStyleBackColor = true;
+            this.SaveUsers.Click += new System.EventHandler(this.SaveUsers_Click);
+            // 
+            // NewUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SaveUsers);
+            this.Controls.Add(this.RoleComboBox);
+            this.Controls.Add(this.textUserPassword);
+            this.Controls.Add(this.textUserEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textUserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textUserId);
+            this.Controls.Add(this.id);
+            this.Name = "NewUser";
+            this.Text = "Email";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label id;
+        private System.Windows.Forms.TextBox textUserId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textUserEmail;
+        private System.Windows.Forms.TextBox textUserPassword;
+        private System.Windows.Forms.ComboBox RoleComboBox;
+        private System.Windows.Forms.Button SaveUsers;
+    }
+}
