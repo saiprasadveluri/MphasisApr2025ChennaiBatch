937fa0eabe34a173466247a2029dbf47f8e9afbb
SwethaAkkarapaku <138562293+SwethaAkkarapaku@users.noreply.github.com>
2025-05-26 17:40:38 +0530

    Add files via upload

---
+++ FoodApp2/LoginForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace FoodApp2
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(222, 51);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(147, 20);
+            this.textEmail.TabIndex = 0;
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Location = new System.Drawing.Point(52, 58);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(32, 13);
+            this.EmailLabel.TabIndex = 1;
+            this.EmailLabel.Text = "Email";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(52, 106);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 2;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(222, 98);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(147, 20);
+            this.textPassword.TabIndex = 3;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(55, 173);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 4;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.textEmail);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Button LoginButton;
+    }
+}
---
+++ FoodApp2/NewLocation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FoodApp2.Classes;
+
+namespace FoodApp2
+{
+    public partial class NewLocation : Form
+    {
+        public DataProvider _dataProvider;
+        public NewLocation(DataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textLocationId.Text) || string.IsNullOrEmpty(textLocationName.Text))
+            {
+                MessageBox.Show("enter location details");
+            }
+            else
+            {
+                Locations loc = new Locations();
+                loc.LocationId = int.Parse(textLocationId.Text);
+                loc.LocationName = textLocationName.Text;
+                _dataProvider.AddLocation(loc);
+                MessageBox.Show("location added successfully");
+                DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+    }
+}
