ba70c9fda272d3770201c1030b88ffe982c8c4f5
BYSANI VENKATA SAI SUKUMAR <136966173+bysanivenkatasaisukumar@users.noreply.github.com>
2025-05-26 21:41:24 +0530

    Add files via upload

---
+++ FoodDelApp(26-05-2025)/FoodApp2/AdminDashboard.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace FoodApp2
+{
+    partial class AdminDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addLocationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LocationsGrid = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.UsersGrid = new System.Windows.Forms.DataGridView();
+            this.addRestaurantToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.RestaurantGrid = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LocationsGrid)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UsersGrid)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RestaurantGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addLocationToolStripMenuItem,
+            this.addUserToolStripMenuItem,
+            this.addRestaurantToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addLocationToolStripMenuItem
+            // 
+            this.addLocationToolStripMenuItem.Name = "addLocationToolStripMenuItem";
+            this.addLocationToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.addLocationToolStripMenuItem.Text = "Add Location";
+            this.addLocationToolStripMenuItem.Click += new System.EventHandler(this.addLocationToolStripMenuItem_Click);
+            // 
+            // addUserToolStripMenuItem
+            // 
+            this.addUserToolStripMenuItem.Name = "addUserToolStripMenuItem";
+            this.addUserToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.addUserToolStripMenuItem.Text = "Add User";
+            this.addUserToolStripMenuItem.Click += new System.EventHandler(this.addUserToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LocationsGrid);
+            this.groupBox1.Location = new System.Drawing.Point(39, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(338, 160);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Locations";
+            // 
+            // LocationsGrid
+            // 
+            this.LocationsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.LocationsGrid.Location = new System.Drawing.Point(26, 29);
+            this.LocationsGrid.Name = "LocationsGrid";
+            this.LocationsGrid.Size = new System.Drawing.Size(287, 114);
+            this.LocationsGrid.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.UsersGrid);
+            this.groupBox2.Location = new System.Drawing.Point(39, 207);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(730, 134);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Users";
+            // 
+            // UsersGrid
+            // 
+            this.UsersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UsersGrid.Location = new System.Drawing.Point(26, 20);
+            this.UsersGrid.Name = "UsersGrid";
+            this.UsersGrid.Size = new System.Drawing.Size(361, 95);
+            this.UsersGrid.TabIndex = 0;
+            // 
+            // addRestaurantToolStripMenuItem
+            // 
+            this.addRestaurantToolStripMenuItem.Name = "addRestaurantToolStripMenuItem";
+            this.addRestaurantToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.addRestaurantToolStripMenuItem.Text = "Add Restaurant";
+            this.addRestaurantToolStripMenuItem.Click += new System.EventHandler(this.addRestaurantToolStripMenuItem_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.RestaurantGrid);
+            this.groupBox3.Location = new System.Drawing.Point(414, 27);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(343, 174);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Restaurants";
+            // 
+            // RestaurantGrid
+            // 
+            this.RestaurantGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.RestaurantGrid.Location = new System.Drawing.Point(21, 29);
+            this.RestaurantGrid.Name = "RestaurantGrid";
+            this.RestaurantGrid.Size = new System.Drawing.Size(278, 114);
+            this.RestaurantGrid.TabIndex = 0;
+            // 
+            // AdminDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "AdminDashboard";
+            this.Text = "AdminDashboard";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AdminDashboard_FormClosed);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.LocationsGrid)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.UsersGrid)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.RestaurantGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addLocationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addUserToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView LocationsGrid;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView UsersGrid;
+        private System.Windows.Forms.ToolStripMenuItem addRestaurantToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView RestaurantGrid;
+    }
+}
---
+++ FoodDelApp(26-05-2025)/FoodApp2/Classes/DataProvider.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FoodApp2.Classes
+{
+    public class DataProvider
+    {
+        private const string USER_FILE_NAME = "UserInfo.txt";
+        private const string RESTAURANT_FILE_NAME = "RestInfo.txt";
+        private const string ORDER_FILE_NAME = "Order.txt";
+        private const string LOCATION_FILE_NAME = "Location.txt";
+
+        public List<Users> _users;
+        public List<Locations> _locations;
+        public List<Restaurant> _restaurants;
+        public List<Order> _orders;
+        public DataProvider()
+        {
+            _users = new List<Users>();
+            _locations = new List<Locations>();
+            _restaurants= new List<Restaurant>();
+            _orders= new List<Order>();
+        }
+        private static DataProvider _instance;
+        public static DataProvider instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new DataProvider();
+                return _instance;
+            }
+        }
+
+
+        //loading the data from the file.text
+        public void LoadData()
+        {
+            LoadUserDetails(USER_FILE_NAME);
+            LoadRestaurantDetails(RESTAURANT_FILE_NAME);
+            //LoadOrderDetails(ORDER_FILE_NAME);
+            LoadLocations(LOCATION_FILE_NAME);
+        }
+        private void LoadUserDetails(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                return;
+            }
+            StreamReader streamReader = new StreamReader(filename);
+            string JSonString = streamReader.ReadToEnd();
+            streamReader.Close();
+            _users = JsonSerializer.Deserialize<List<Users>>(JSonString);
+        }
+        private void LoadRestaurantDetails(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                return;
+            }
+            StreamReader streamReader = new StreamReader(filename);
+            string JSonString = streamReader.ReadToEnd();
+            streamReader.Close();
+            _restaurants = JsonSerializer.Deserialize<List<Restaurant>>(JSonString);
+
+        }
+        //private void LoadOrderDetails(string filename)
+        //{
+        //    StreamReader streamReader = new StreamReader(filename);
+        //    string JSonString = streamReader.ReadToEnd();
+        //    streamReader.Close();
+        //    _orders = JsonSerializer.Deserialize<List<Order>>(JSonString);
+        //}
+        public bool Verify(string email,string password)
+        {
+            Users temp = _users.Find(ui => ui.Email == email && ui.Password == password);
+            if (temp != null)
+            {
+                //if(_users.Role)
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void LoadLocations(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                return;
+            }
+            StreamReader streamReader = new StreamReader(filename);
+            string JSonString = streamReader.ReadToEnd();
+            streamReader.Close();
+            _locations = JsonSerializer.Deserialize<List<Locations>>(JSonString);
+        }
+
+        //saving details
+        private void SaveLocationDetails(string filename)
+        {
+            string JSonString = JsonSerializer.Serialize<List<Locations>>(_locations);
+            StreamWriter streamWriter = new StreamWriter(filename);
+            streamWriter.Write(JSonString);
+            streamWriter.Close();
+        }
+        private void SaveUserDetails(string filename)
+        {
+            string JSonString = JsonSerializer.Serialize<List<Users>>(_users);
+            StreamWriter streamWriter = new StreamWriter(filename);
+            streamWriter.Write(JSonString);
+            streamWriter.Close();
+        }
+        private void SaveRestaurantDetails(string filename)
+        {
+            string JSonString = JsonSerializer.Serialize<List<Restaurant>>(_restaurants);
+            StreamWriter streamWriter = new StreamWriter(filename);
+            streamWriter.Write(JSonString);
+            streamWriter.Close();
+        }
+        private void SaveOrderDetails(string filename)
+        {
+            string JSonString = JsonSerializer.Serialize<List<Order>>(_orders);
+            StreamWriter streamWriter = new StreamWriter(filename);
+            streamWriter.Write(JSonString);
+            streamWriter.Close();
+        }
+
+        //Load and Save()
+        public void SaveToFile()
+        {
+            SaveUserDetails(USER_FILE_NAME);
+            SaveRestaurantDetails(RESTAURANT_FILE_NAME);
+            SaveOrderDetails(ORDER_FILE_NAME);
+            SaveLocationDetails(LOCATION_FILE_NAME);
+        }
+
+
+        //crud operations
+
+        public List<Locations> GetAllLocations()
+        {
+            return _locations;
+        }
+
+        public bool AddLocation(Locations location)
+        {
+            var Res = _locations.Find(loc => loc.LocationName == location.LocationName);
+            if (Res == null)
+            {
+                _locations.Add(location);
+                return true;
+            }
+            return false;
+        }
+        public List<Users> GetAllUsers()
+        {
+            return _users;
+        }
+
+        //public List<Owner> GetAllOwners()
+        //{
+        //    return _userInfo.OfType<Owner>().ToList();
+        //}
+
+        public bool AddUser(Users userInfo)
+        {
+            Users temp = _users.Find(ui => ui.Email == userInfo.Email);
+            if (temp == null)
+            {
+                _users.Add(userInfo);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AddRestaurant(Restaurant restaurant)
+        {
+            var Res = _restaurants.Find(r => r.RestaurantName == restaurant.RestaurantName && r.RestLocation.LocationName == restaurant.RestLocation.LocationName);
+            if (Res == null)
+            {
+                _restaurants.Add(restaurant);
+                return true;
+            }
+            return false;
+        }
+
+        public List<Restaurant> GetRestaurants()
+        {
+            return _restaurants;
+        }
+
+        public List<Restaurant> GetRestaurantsByLocation(string LocName)
+        {
+            return _restaurants.Where(r => r.RestLocation.LocationName == LocName).ToList();
+        }
+
+        public List<Restaurant> GetRestaurantsByOwner(string Email)
+        {
+            return _restaurants.Where(r => r.RestOwner.Email == Email).ToList();
+        }
+
+        public bool PlaceOrder(Users custObj, List<OrderMenuEntry> orderdMenu, Restaurant restaurant)
+        {
+            Order order = new Order(restaurant, orderdMenu, custObj);
+            _orders.Add(order);
+            return true;
+        }
+
+        public List<Order> GetOrderList()
+        {
+            return _orders;
+        }
+
+        public List<Order> GetOrderByCustomer(String email)
+        {
+            return _orders.Where(o => o.OrderBy.Email == email).ToList();
+        }
+
+        public List<Order> GetOrderByRestaurant(Restaurant restaurant)
+        {
+            return _orders.Where(o => o.Restaurant == restaurant).ToList();
+        }
+    }
+}
