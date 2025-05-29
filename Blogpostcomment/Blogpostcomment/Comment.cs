baed65fd42b2dceeaabf7224f843fadb5393e577
Sona970 <sona.shamshuddin@mphasis.com>
2025-05-29 18:02:28 +0530

    Add files via upload

---
+++ Blogpostcomment/Blogpostcomment/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogpostcomment
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public int BlogPostId { get; set; }
+        public required string CommentTitle { get; set; }
+        public required string CommentText { get; set; }
+        public required string CommentBy { get; set; }
+    }
+}
