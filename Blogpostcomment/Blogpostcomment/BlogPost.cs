7a6d562ef2def04ba1457aacf742c66b901b0b4c
vamsidamerla <vamsivikky7@gmail.com>
2025-05-29 21:56:44 +0530

    Add files via upload

---
+++ Blogpostcomment/Blogpostcomment/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogpostcomment
+{
+    public class BlogPost
+    {
+        public int BlogPostId { get; set; }
+        public required string BlogTitle { get; set; }
+        public required string BlogText { get; set; }
+        public required string PostedBy { get; set; }
+    }
+}
---
+++ Blogpostcomment/Blogpostcomment/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information  
+
+using Blogpostcomment;
+using Microsoft.Data.SqlClient;
+//using DataAccessLayer;
+using Microsoft.SqlServer.Server;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ADONetBlogWinApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //DataAccess ds = new DataAccess();
+            //ds.DeletePost(2);
+            //ds.GetAllPosts();
+            ////ds.AddPost("Science", "Science—the fascinating pursuit of understanding the universe! Science is a systematic discipline that builds and organizes knowledge through observation, experimentation, and reasoning.", "Sona");
+            //ds.GetAllPosts();
+            DataComment dataComment = new DataComment();
+            //dataComment.AddComment(1,"Science", "Science—the fascinating pursuit of understanding the universe! Science is a systematic discipline that builds and organizes knowledge through observation, experimentation, and reasoning.", "Sona");
+            //dataComment.GetAllPosts();
+            dataComment.DeletePost(2);
+        
+        }
+    }
+}
