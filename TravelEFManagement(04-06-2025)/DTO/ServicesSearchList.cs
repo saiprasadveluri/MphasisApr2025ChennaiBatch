661a690e58d01f2699efe24fc335dc71a8e539ac
SwethaAkkarapaku <138562293+SwethaAkkarapaku@users.noreply.github.com>
2025-06-05 00:46:37 +0530

    Add files via upload

---
+++ TravelEFManagement(04-06-2025)/DTO/ServicesSearchList.cs
@@ -0,0 +1,6 @@
+public class ServicesSearchList
+{
+    public string ServiceTypeName { get; set; }
+    public double PricePerKm { get; set; }
+    public double Distance { get; set; }
+}
---
+++ TravelEFManagement(04-06-2025)/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+DataAccess dataAccess=new DataAccess();
+
+//View All Data
+// List<Location> locationsList=dataAccess.GetAllLocations();
+// foreach(var data in locationsList)
+// {
+//     Console.WriteLine($"Location Id: {data.LocationId} and Location Name: {data.LocationName}");
+// }
+
+// List<ServiceType> serviceTypesList=dataAccess.GetAllServiceTypes();
+// foreach(var data in serviceTypesList)
+// {
+//     Console.WriteLine($"Service Type Id: {data.STypeId}, Service Type Name: {data.ServiceTypeName}, Price Per Km: {data.PricePerKm}");
+// }
+
+//Add Data
+
+// dataAccess.AddLocation(4,"Bangalore");
+// foreach(var data in locationsList)
+// {
+//     Console.WriteLine($"Location Id: {data.LocationId} and Location Name: {data.LocationName}");
+// }
+
+// dataAccess.AddService(1,2,3,12);
+// List<Service> serviceList=dataAccess.GetAllServices();
+// foreach(var data in serviceList)
+// {
+//     Console.WriteLine($"ServiceId: {data.ServiceId}, SerTypeId: {data.SerTypeId}, SourceLocId: {data.SourceLocId}, DestLocId: {data.DestLocId}, Distance: {data.Distance}");
+// }
+
+// dataAccess.AddBooking(1,DateTime.Parse("2025-06-04"),4,"user1");
+// List<Booking> bookingList=dataAccess.GetAllBookings();
+// foreach(var data in bookingList)
+// {
+//     Console.WriteLine($"BookId: {data.BookId}, ServiceId: {data.ServiceId}, TravelDate: {data.TravelDate}, SeatCount: {data.SeatCount}, BookBy: {data.BookBy}");
+// }
