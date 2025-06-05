d80178699b8543f1c891352d8fd86d33ded52a0c
Sravani2712 <mittapallysravani2710@gmail.com>
2025-06-05 09:01:11 +0530

    Add files via upload

---
+++ TravelEzeeEFConsole(04-06-25)/TravelEzeeEFConsole/Migrations/20250604100129_Location.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelEzeeEFConsole.Migrations
+{
+    /// <inheritdoc />
+    public partial class Location : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    LocationId = table.Column<long>(type: "bigint", nullable: false),
+                    LocationName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    LocationDescription = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.LocationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Servicetypes",
+                columns: table => new
+                {
+                    STypeId = table.Column<long>(type: "bigint", nullable: false),
+                    ServiceTypeName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PricePerKm = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Servicetypes", x => x.STypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    ServiceId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServiceTypeId = table.Column<long>(type: "bigint", nullable: false),
+                    SourceLocId = table.Column<long>(type: "bigint", nullable: false),
+                    DestLocId = table.Column<long>(type: "bigint", nullable: false),
+                    Distance = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.ServiceId);
+                    table.ForeignKey(
+                        name: "FK_Services_Locations_DestLocId",
+                        column: x => x.DestLocId,
+                        principalTable: "Locations",
+                        principalColumn: "LocationId");
+                    table.ForeignKey(
+                        name: "FK_Services_Locations_SourceLocId",
+                        column: x => x.SourceLocId,
+                        principalTable: "Locations",
+                        principalColumn: "LocationId");
+                    table.ForeignKey(
+                        name: "FK_Services_Servicetypes_ServiceTypeId",
+                        column: x => x.ServiceTypeId,
+                        principalTable: "Servicetypes",
+                        principalColumn: "STypeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    BookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ServiceId = table.Column<long>(type: "bigint", nullable: false),
+                    TravelDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SeatCount = table.Column<int>(type: "int", nullable: false),
+                    BookBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.BookId);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "ServiceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_ServiceId",
+                table: "Bookings",
+                column: "ServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_DestLocId",
+                table: "Services",
+                column: "DestLocId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_ServiceTypeId",
+                table: "Services",
+                column: "ServiceTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_SourceLocId",
+                table: "Services",
+                column: "SourceLocId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Servicetypes_ServiceTypeName",
+                table: "Servicetypes",
+                column: "ServiceTypeName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Servicetypes");
+        }
+    }
+}
---
+++ TravelEzeeEFConsole(04-06-25)/TravelEzeeEFConsole/Migrations/20250604101259_Sloca.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TravelEzeeEFConsole.Migrations
+{
+    /// <inheritdoc />
+    public partial class Sloca : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Locations",
+                columns: new[] { "LocationId", "LocationDescription", "LocationName" },
+                values: new object[,]
+                {
+                    { 1L, null, "HYD" },
+                    { 2L, null, "MLG" },
+                    { 3L, null, "SRPT" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Servicetypes",
+                columns: new[] { "STypeId", "PricePerKm", "ServiceTypeName" },
+                values: new object[,]
+                {
+                    { 1L, 12.5, "Express" },
+                    { 2L, 12.9, "Luxury" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Servicetypes",
+                keyColumn: "STypeId",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Servicetypes",
+                keyColumn: "STypeId",
+                keyValue: 2L);
+        }
+    }
+}
