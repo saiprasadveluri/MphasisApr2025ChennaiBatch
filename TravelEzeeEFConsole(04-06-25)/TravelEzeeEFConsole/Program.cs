21c3ad78422ac7730eae0d90f94bb57a63518854
Sravani2712 <mittapallysravani2710@gmail.com>
2025-06-05 09:10:43 +0530

    Add files via upload

--- TravelEzeeEFConsole(04-06-25)/TravelEzeeEFConsole/Program.cs
+++ TravelEzeeEFConsole(04-06-25)/TravelEzeeEFConsole/Program.cs
@@ -1,4 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 DataAccess dataAccess = new DataAccess();
-var Lst = dataAccess.GetAllServicesView();
+var List = dataAccess.GetAllLocations();
+foreach(var data in List)
+{
+    Console.WriteLine($"Location Id : {data.LocationId} and Location Name:{data.LocationName}");
+}
+ var Service = dataAccess.GetAllServiceTypes();
+ foreach(var data in Service)
+ {
+    Console.WriteLine($"STypeId:{data.STypeId}-ServiceTypeName:{data.ServiceTypeName}-PricePerKm:{data.PricePerKm}");
+ }
+ //Add Dataa
+//  dataAccess.AddLocation(4,"Pune",".Net Batch");
+//  dataAccess.AddLocation(5,"Pune",".Net Batch2");
+
+ //Add Service
+ dataAccess.AddService(1,2,3,12);
+var service =dataAccess.GetAllServices();
+foreach(var data in service )
+{
+   
+Console.WriteLine($"ServiceId:{data.ServiceId}-SerTypeId:{data.SerTypeId}-SourceLocId:{data.SourceLocId}-DestLocId:{data.DestLocId}-Distance:{data.Distance}");
+}
+
+//Add Booking
+dataAccess.AddBooking(1,DateTime.Parse("2025-06-04"),4,"Neha");
+var book = dataAccess.GetAllBookings();
+foreach(var data in book)
+{
+    Console.WriteLine($"BookId:{data.BookId}-ServiceId:{data.ServiceId}-TravelDate:{data.TravelDate}-SeatCount:{data.SeatCount}-BookBy:{data.BookBy}");
+}
