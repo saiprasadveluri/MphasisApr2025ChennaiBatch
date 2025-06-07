9d4f358424949d1f6b63dd6f06761c9ef1aaac25
Trinay5 <113661792+Trinay5@users.noreply.github.com>
2025-06-07 17:03:25 +0530

    Add files via upload

---
+++ June 7th - TravelEzeeWinUI/TravelEzeeDataAccessLayer/Data/Booking.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TravelEzeeDataAccessLayer;
+public class Booking
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+ 
+    public Guid BookId { get; set; }
+    [ForeignKey(nameof(TravelService))]
+    public long ServiceId { get; set; }
+ 
+    [Required]
+    public DateTime TravelDate { get; set; }
+    public int SeatCount { get; set; }
+    public string? BookBy { get; set; }
+ 
+    //Navigation Props
+    public Service? TravelService { get; set; }
+ 
+}
---
+++ June 7th - TravelEzeeWinUI/TravelEzeeWinUI/AddLocationDialogue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelEzeeWinUI
+{
+    public partial class AddLocationDialogue : Form
+    {
+        DataAccess dataAccess;
+        public AddLocationDialogue()
+        {
+            dataAccess = new DataAccess();
+            InitializeComponent();
+        }
+
+        private void txtDescription_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddloc_Click(object sender, EventArgs e)
+        {
+            //Get Available Max Id and Add Location DB.
+            long NextLocationId = 1;
+            var locationlist=dataAccess.GetAllLocations();
+            if (locationlist.Count > 0)
+            {
+                long AvailableMaxId=locationlist.Max(l=>l.LocationId);
+                NextLocationId = AvailableMaxId + 1;
+            }
+            string locName=txtLocation.Text;
+            string locDescription = txtDescription.Text;
+            bool Status=dataAccess.AddLocation(NextLocationId, locName, locDescription);
+            if (Status)
+            {
+                MessageBox.Show("success in adding new location");
+            }
+            else
+            {
+                MessageBox.Show("Error in adding new location");
+            }
+        }
+    }
+}
