ff364d05c243517e30de9b756e34bb7797215dc4
Rajeevbobby <125657045+RajeevBobby@users.noreply.github.com>
2025-06-24 11:47:04 +0530

    Add files via upload

---
+++ api/Migrations/20250623115010_AddDriverEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RideAggregatorApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDriverEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ api/Services/IRideService.cs
@@ -0,0 +1,14 @@
+﻿using RideAggregatorApi.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RideAggregatorApi.Services
+{
+    public interface IRideService
+    {
+        Task<List<Ride>> GetAllAsync();
+        Task<Ride?> GetByIdAsync(int id);
+        Task<List<Ride>> GetByCustomerIdAsync(int customerId);
+        Task<List<Ride>> GetByDriverIdAsync(int driverId);
+    }
+
+}
