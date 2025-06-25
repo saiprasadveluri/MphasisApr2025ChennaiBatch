4f9d4f031cc768e01c1c3c1d339a7c7854a800a0
ShivamMphasis <shivam.kumar11@mphasis.com>
2025-06-25 18:58:35 +0530

    Add files via upload

---
+++ RIDEAPP/RideAggregator.web/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace RideAggregator.web.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
