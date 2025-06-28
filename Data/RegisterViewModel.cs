6df0b17509d6c60e27680b63abb2a49e7e5c4eed
shrinidhisaravanan1011 <shrinidhisaravanan1011@gmail.com>
2025-06-28 12:57:07 +0530

    Add files via upload

---
+++ Data/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineQuizApp.Data
+{
+    public class RegisterViewModel
+    {
+        [Key]
+        [Required]
+        public Guid RegisterId { get; set; }
+        [Required(ErrorMessage = "UserEmail is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]//(ErrorMessage = "Captcha is required")
+        public string CaptchaInput { get; set; }
+    }
+}
+    
---
+++ OnlineQuizApp/OnlineQuizApp/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineQuizApp.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult RegisterLogin()
+        {
+            return View();
+        }
+    }
+}
