5758330140c68a2ce04e55924cd4d1df8110b7a7
PremKumarRevalla <111066517+PremKumarRevalla@users.noreply.github.com>
2025-06-28 11:38:53 +0530

    Add files via upload

---
+++ Logout/IndexController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineQuiz.Controllers
+{
+    public class IndexController : Controller
+    {
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Logout")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogoutPost()
+        {
+            HttpContext.Session.Clear();
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Register", "NewCaptcha");
+        }
+
+    }
+}
+
