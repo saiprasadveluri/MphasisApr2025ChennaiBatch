de9e63a158f8e15242afe52697fa2a0516cdc339
Nikhila112001 <nasina.nikhila2001@gmail.com>
2025-06-28 12:19:53 +0530

    Add files via upload

---
+++ DTO/AccountDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineQuizApplicationAPI.DTO
+{
+    public class AccountDTO
+    {
+
+        public Guid AccountId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineQuizApplicationAPI.Models
+{
+    public class UserInfo
+    {
+        [Key]
+        public Guid UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [ForeignKey(nameof(accountData))]
+        public Guid AccountId { get; set; }
+        public string ContactNo { get; set; }
+
+        //Navigation Property
+        public Account accountData { get; set; }
+        public ICollection<Quiz> quizzes { get; set; }
+    }
+}
