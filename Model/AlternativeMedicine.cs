19949c20a6e457f281a01ff68be43408d1eed5e8
PranavReddy-Athivarapu <98316161+PranavReddy-Athivarapu@users.noreply.github.com>
2025-06-28 12:28:03 +0530

    Add files via upload

---
+++ Model/AlternativeMedicine.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlinePharmacyAppAPI.Model
+{
+    public class AlternativeMedicine
+    {
+        [Key]
+        public int AlternativeId { get; set; }
+
+        [ForeignKey(nameof(OriginalMedicine))]
+        public int OriginalMedicineId { get; set; }
+
+        [ForeignKey(nameof(SubstituteMedicine))]
+        public int SubstituteMedicineId { get; set; }
+
+        // Navigation
+        public virtual Medicine OriginalMedicine { get; set; }
+        public virtual Medicine SubstituteMedicine { get; set; }
+    }
+}
---
+++ Model/Discount.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlinePharmacyAppAPI.Model
+{
+    public class Discount
+    {
+        public int DiscountId { get; set; }
+
+        [Required, StringLength(50)]
+        public string DiscountCode { get; set; }
+
+        [Required]
+        public string DiscountType { get; set; } 
+
+        [Required]
+        public decimal Value { get; set; } 
+
+        [Required]
+        public bool IsPercentage { get; set; } = true;
+
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+        public DateTime? EndDate { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
