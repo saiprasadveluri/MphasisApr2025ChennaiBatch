089df88e298a2940246f52f44887e75be6def862
shrinidhisaravanan1011 <shrinidhisaravanan1011@gmail.com>
2025-06-28 17:28:37 +0530

    Add files via upload

---
+++ DTO/RegisterViewModeldto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineQuizApp.DTO
+{
+    public class RegisterViewModeldto
+    {
+        public Guid RegisterId { get; set; }
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        [Required]//(ErrorMessage = "Captcha is required")
+        public string CaptchaInput { get; set; }
+        public string CaptchaOutput { get; set; }
+
+    }
+}
