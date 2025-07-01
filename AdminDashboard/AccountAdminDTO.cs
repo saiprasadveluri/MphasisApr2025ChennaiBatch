6790ef401dd2eb8f3c3abb383bc44fadc62fe7f9
shrinidhisaravanan1011 <shrinidhisaravanan1011@gmail.com>
2025-07-01 11:16:56 +0530

    Add files via upload

---
+++ AdminDashboard/AccountAdminDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineQuizApp.DTO
+{
+    public class AccountAdminDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string AdminName { get; set; }
+        public string ContactNo { get; set; }
+    }
+}
---
+++ AdminDashboard/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineQuizApp.DTO;
+
+namespace OnlineQuizApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        AccountAdminDTO currentadmin = new AccountAdminDTO
+        {
+            Email = "admin@example.com",
+            Password = "admin123",
+            AdminName = "Super Admin",
+            ContactNo = "9876543210"
+        };
+        public IActionResult Profile()
+        {
+            return View(currentadmin);
+        }
+        public IActionResult EditProfile()
+        {
+            return View(currentadmin);
+        }
+    }
+}
