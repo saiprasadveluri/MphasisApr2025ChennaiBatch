045d8c27a6d0c48916181a1d1ee2d72f5494c629
Sona970 <sona.shamshuddin@mphasis.com>
2025-07-01 10:33:33 +0530

    Add files via upload

---
+++ Generic/GenericRepositoryMVC.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobSearchMVC
+{
+    public class GenericRepositoryMVC<TEntity> where TEntity : class
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _endpoGuid;
+        public GenericRepositoryMVC(HttpClient httpClient, string endpoGuid)
+        {
+            _httpClient = httpClient;
+            _endpoGuid = endpoGuid;
+            _httpClient.BaseAddress = new Uri("https://localhost:7103/api/");
+        }
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            var response = await _httpClient.GetAsync(_endpoGuid);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new InvalidOperationException("Entity not found");
+            }
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<IEnumerable<TEntity>>();
+            }
+            return Enumerable.Empty<TEntity>();
+        }
+        //[HttpGet("{id}")]
+        //public async Task<TEntity> GetByIdAsync(string id)
+        //{
+        //    var response = await _httpClient.GetAsync($"{_endpoGuid}/{id}");
+        //    if (!response.IsSuccessStatusCode)
+        //    {
+        //        throw new InvalidOperationException("Entity not found");
+        //    }
+
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        return await response.Content.ReadFromJsonAsync<TEntity>();
+        //    }
+        //    return null;
+        //}
+        public async Task<TEntity?> GetByIdAsync(string id)
+        {
+            var response = await _httpClient.GetAsync($"{_endpoGuid}/{id}");
+
+            if (!response.IsSuccessStatusCode)
+                return default;
+
+            return await response.Content.ReadFromJsonAsync<TEntity>();
+        }
+
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            var response = await _httpClient.PostAsJsonAsync(_endpoGuid, entity);
+            return entity;
+        }
+
+        public async Task<TEntity> UpdateAsync(string id, TEntity entity)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"{_endpoGuid}/{id}", entity);
+            return entity;
+        }
+
+        public async Task<TEntity> DeleteAsync(string id, TEntity entity)
+        {
+            var response = await _httpClient.DeleteAsync($"{_endpoGuid}/{id}");
+            return entity;
+        }
+    }
+}
---
+++ Generic/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using JobSearchMVC.DataDTO;
+
+namespace JobSearchMVC.Models
+{
+    public class UnitOfWork
+    {
+        public readonly GenericRepositoryMVC<UserDTO> userRepository;
+        public readonly UserMVCRepo userMVCRepo;
+        public readonly GenericRepositoryMVC<WorkExperienceDTO> workRepo;
+        public readonly GenericRepositoryMVC<CandidateDTO> candidateRepo;
+        public readonly GenericRepositoryMVC<EmployerDTO> employerRepo;
+        public readonly GenericRepositoryMVC<EducationDTO> educationRepo;
+        public readonly GenericRepositoryMVC<WorkExperienceDTO> workexpRepo;
+        public UnitOfWork(HttpClient httpClient)
+        {
+            // You can define the endpoint segments here
+            userMVCRepo = new UserMVCRepo(httpClient);
+            userRepository = new GenericRepositoryMVC<UserDTO>(httpClient, "User");
+            workRepo = new GenericRepositoryMVC<WorkExperienceDTO>(httpClient, "WorkExperience");
+            candidateRepo = new GenericRepositoryMVC<CandidateDTO>(httpClient, "Candidates");
+            employerRepo = new GenericRepositoryMVC<EmployerDTO>(httpClient, "Employer");
+            educationRepo = new GenericRepositoryMVC<EducationDTO>(httpClient, "Education");
+            workexpRepo = new GenericRepositoryMVC<WorkExperienceDTO>(httpClient, "WorkExperience");
+        }
+
+    }
+}
