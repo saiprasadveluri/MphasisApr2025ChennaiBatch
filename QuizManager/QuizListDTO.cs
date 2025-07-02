30282f157d4cde35caf5d1b20c0abedd6fb0732a
shrinidhisaravanan1011 <shrinidhisaravanan1011@gmail.com>
2025-07-02 09:37:44 +0530

    Add files via upload

---
+++ QuizManager/QuizListDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineQuizApp.DTO
+{
+    public class QuizListDTO
+    {
+            public Guid QuizId { get; set; }
+            public string QuizTitle { get; set; }
+            public string Category { get; set; }
+            public int Duration { get; set; }
+            public bool IsScheduled { get; set; }
+
+            [DataType(DataType.DateTime)]
+            public DateTime? ScheduledTime { get; set; }
+            
+        }
+    }
+
---
+++ QuizManager/QuizManagerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineQuizApp.DTO;
+
+namespace OnlineQuizApp.Controllers
+{
+    public class QuizManagerController : Controller
+    {
+        public IActionResult QuizList()
+        {
+            var sampleQuizzes = new List<QuizListDTO>
+           {
+             new QuizListDTO
+             {
+                    QuizId = Guid.NewGuid(),
+                    QuizTitle = "C# Basics",
+                    Category = "Programming",
+                    Duration = 30
+             }
+        };
+            return View(sampleQuizzes);
+        }
+        public IActionResult EditQuiz()
+        {
+            return View();
+        }
+        public IActionResult AddNewQuiz()
+        {
+            return View();  
+        }
+    }
+}
