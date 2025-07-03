5d90e46c532a17d36776074527723bfb02b85d12
PranavReddy-Athivarapu <98316161+PranavReddy-Athivarapu@users.noreply.github.com>
2025-07-03 15:33:10 +0530

    Delete API Initial Main directory

--- API Initial Main/DTO/CheckoutResultDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlinePharmacyAppAPI.DTO
-{
-    public class CheckoutResultDTO
-    {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-        public decimal FinalAmount { get; set; }
-    }
-}
